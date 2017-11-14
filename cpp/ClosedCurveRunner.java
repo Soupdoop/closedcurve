de429601866e741131d106762da2a72e1105ed2b
Jack Herd <jherd23@gmail.com>
2017-11-14 15:43:54 -0500

    Some changes for running.

--- cpp/ClosedCurveRunner.java
+++ cpp/ClosedCurveRunner.java
@@ -145,28 +145,10 @@ public class ClosedCurveRunner extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		ClosedCurveRunner ccr = new ClosedCurveRunner();
-		/*if(args.length < 2) {
-			System.err.println("Invalid input –– should have both an input image name and an output image name");
-			return;
-		}
 		try {
-			PrintWriter pointWriter = new PrintWriter(".points");
-			File imgFile = new File(args[0]);
-      BufferedImage img = ImageIO.read(imgFile);
-      CC2O cc2o = new CC2O(img);
-			cc2o.print(pointWriter);
-			pointWriter.close();
-			Process two_opt = Runtime.getRuntime().exec("./sorter.exe .points .spoints");
-			two_opt.waitFor();
-			CC2O goodPoints = new CC2O(".spoints");
-			ImageIO.write(goodPoints.draw(), "png", new File(args[1]));
-			File p = new File(".points");
-			p.delete();
-			File sp = new File(".spoints");
-			sp.delete();
+			ClosedCurveRunner ccr = new ClosedCurveRunner();
 		} catch(Exception e) {
-			e.printStackTrace();
-		}*/
+			JOptionPane.showMessageDialog(null, "Error in launching!", "IO Error", JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
